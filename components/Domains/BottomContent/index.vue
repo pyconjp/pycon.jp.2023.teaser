5785236662419cea24265c8e886410907b5646df
Yoichi TAKAI <takai@cmscom.jp>
2022-03-17 11:56:45 +0900

    :lipstick: Modify footer

--- components/Domains/BottomContent/index.vue
+++ components/Domains/BottomContent/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bottom-content py-24">
+  <div class="py-24 bottom-content">
     <div class="text-center">
       <p class="text-2xl en-title-font">Contact</p>
       <div class="mt-10">
@@ -7,15 +7,15 @@
           <p>ボランティアによる運営ですので、回答には1週間程度を要します。</p>
           <p>何卒お待ちいただけますようにお願いします。</p>
         </div>
-        <div class="text-sm en-regular-font mt-4">
-          <p>It is run by volunteers. It takes about a week to answer.</p>
+        <div class="mt-4 text-sm en-regular-font">
+          <p>PyCon JP is organized by volunteers, so it may take a week for us to reply to inquiries.</p>
           <p>Thank you for your patience.</p>
         </div>
       </div>
       <div class="flex justify-center contact-button-area">
         <a
           href="mailto:pyconjp@pycon.jp"
-          class="en-regular-font border text-white bg-auto border-white hover:text-black hover:bg-white rounded-full contact-button"
+          class="text-white bg-auto border border-white rounded-full en-regular-font hover:text-black hover:bg-white contact-button"
         >
           <span>pyconjp@pycon.jp</span>
         </a>
