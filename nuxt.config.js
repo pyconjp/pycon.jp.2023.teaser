644d3ea905f8da0b6621d4c3fba0b425a0950baf
Yoichi Takai <contact@peacock0803sz.com>
2023-04-12 02:43:06 +0900

    fix: Correct missing replacing

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PyCon JP 2023',
+    title: 'PyCon APAC 2023',
     htmlAttrs: {
       lang: 'ja',
     },
@@ -23,11 +23,11 @@ export default {
       {
         hid: 'og:site_name',
         property: 'og:site_name',
-        content: 'PyCon JP 2023',
+        content: 'PyCon APAC 2023',
       },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://2023.pycon.jp' },
-      { hid: 'og:title', property: 'og:title', content: 'PyCon JP 2023' },
+      { hid: 'og:url', property: 'og:url', content: 'https://2023-apac.pycon.jp' },
+      { hid: 'og:title', property: 'og:title', content: 'PyCon APAC 2023' },
       {
         hid: 'og:description',
         property: 'og:description',
